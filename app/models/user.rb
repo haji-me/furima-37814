587e086d52733c7d22ede1b58317bbf642df4df7
haji-me <harusugasuga01180118@gmail.com>
2022-07-28 17:51:34 +0900

    ユーザー管理機能の実装

---
+++ app/models/user.rb
@@ -0,0 +1,22 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+         kanji = /\A[一-龥]+\z/
+         katakana = /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/
+
+         validates :nickname,          presence: true
+         validates :password,          format: { with: PASSWORD_REGEX },
+                                       confirmation: true
+         validates :last_name,         presence: true,
+                                       format: { with: kanji }
+         validates :first_name,        presence: true,
+                                       format: { with: kanji }
+         validates :last_name_kana,    presence: true,
+                                       format: { with: katakana }
+         validates :first_name_kana,   presence: true,
+                                       format: { with: katakana }
+         validates :birth,             presence: true
+end
